db62d1c1fd728763f8f64b179d2f6e0c15701e6d
Greatoxy1 <156723421+Greatoxy1@users.noreply.github.com>
2026-03-05 06:25:35 +0100

    update europe news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,27 +1,37 @@
-export default function News (){
-    return(
+export default function News() {
+    return (
         <div>
             <h2 >
-              🔥  Breaking news🔥
+                🔥  Breaking news🔥
             </h2>
+            <h2>UK evacuating citizens from Middle East</h2>
+            <p>
+                The UK government has chartered evacuation flights for British nationals leaving the Middle East because of escalating conflict in the region.
+
+                Thousands of Britons registered for help,
+                and the first repatriation flight was arranged from Oman.
+                The UK government announced a policy aiming to make flexible working (hybrid work) easier to request.
+
+                By 2027, employers will find it harder to refuse such requests, expanding workers’ rights.
+            </p>
             <h2>
                 Trump is pushing to use UK and EU Military basses
             </h2>
             <p>
-                <img src="eu.png" alt="image" width="400"/>
+                <img src="eu.png" alt="image" width="350" />
             </p>
             <p>
-               Trump has been pushing to use bases in Europe (like in Spain, the UK and Cyprus) to support U.S. military operations related to the conflict with Iran and allied military action.
+                Trump has been pushing to use bases in Europe (like in Spain, the UK and Cyprus) to support U.S. military operations related to the conflict with Iran and allied military action.
                 Because these bases are geographically closer to the Middle East and other hotspots than U.S. bases in the continental United States,
-                 they’re much more useful for launching air operations or moving troops quickly. Without access, the U.S. would have to fly much farther, which costs time, money, and effort.
-                 In recent days, Trump has publicly criticized Spain for refusing to let the U.S. use the bases at Rota and Morón for potential Iranian strikes.
-                 The United Kingdom and its Prime Minister Keir Starmer for initially delaying or restricting U.S. use of British facilities — even calling him “no Winston Churchill” and expressing frustration over Britain’s decisions.
-                 His criticism of Spain and the UK isn’t just about the bases themselves but also about what he sees as insufficient defence contributions by European allies.
-                 Trump’s rhetoric also ties into a broader policy goal: he repeatedly demands that European NATO allies increase their defence spending.
-                  He’s been pushing for NATO members to spend much more on defence (some reports say as high as 5% of GDP) — far above the current minimum target of 2%. 
-                 He argues that because the U.S. spends far more than any other NATO country to defend Europe, European partners should contribute more.
-                 🔥 “Trump threatens to halt all trade with Spain” after Madrid refused U.S. military aircraft access to bases in southern Spain for operations
-                  linked to attacks on Iran — escalating tensions sharply between Washington and a key European NATO partner.
+                they’re much more useful for launching air operations or moving troops quickly. Without access, the U.S. would have to fly much farther, which costs time, money, and effort.
+                In recent days, Trump has publicly criticized Spain for refusing to let the U.S. use the bases at Rota and Morón for potential Iranian strikes.
+                The United Kingdom and its Prime Minister Keir Starmer for initially delaying or restricting U.S. use of British facilities — even calling him “no Winston Churchill” and expressing frustration over Britain’s decisions.
+                His criticism of Spain and the UK isn’t just about the bases themselves but also about what he sees as insufficient defence contributions by European allies.
+                Trump’s rhetoric also ties into a broader policy goal: he repeatedly demands that European NATO allies increase their defence spending.
+                He’s been pushing for NATO members to spend much more on defence (some reports say as high as 5% of GDP) — far above the current minimum target of 2%.
+                He argues that because the U.S. spends far more than any other NATO country to defend Europe, European partners should contribute more.
+                🔥 “Trump threatens to halt all trade with Spain” after Madrid refused U.S. military aircraft access to bases in southern Spain for operations
+                linked to attacks on Iran — escalating tensions sharply between Washington and a key European NATO partner.
             </p>
             <p>
                 <a href="https://globbalnews.com">Read More</a>
