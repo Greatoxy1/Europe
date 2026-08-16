41723e9a72c02ffd0d4e1494efac5375b2de3266
Greatoxy1 <156723421+Greatoxy1@users.noreply.github.com>
2026-08-16 22:38:57 +0200

    update news content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
 import News from "./components/News";
+import NewsPage from "./components/NewsPage"
 export function App() {
   return (
     <div className="App">
-    <News/>
+      <NewsPage />
+      <News />
     </div>
   );
 }
+
+export default App;
